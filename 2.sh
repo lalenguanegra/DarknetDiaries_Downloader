9b0834f1ca3bddeb1440ecc0ccc64daa0cadce04
Hunt The Alien <36826771+lalenguanegra@users.noreply.github.com>
2020-04-19 13:26:44 -0700

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "ENTER A URL";
-for i in {1..2}; do curl "https://darknetdiaries.com/episode/$i/" > html.txt;
+for i in {1..63}; do curl "https://darknetdiaries.com/episode/$i/" > html.txt;
 grep -o '"https://traffic.megaphone[^"]*["mp3"]' html.txt | tr -d '"' > result.txt;
 grep -o '"Ep [^"]*["\"\"]' html.txt | tr -d '"' > title.txt;
 rm html.txt;
-while read url; do wget -O $((++i)).mp3 "$url"; done < result.txt;
+while read url; do wget -O $((i)).mp3 "$url"; done < result.txt;
 while read title; do mv "$i.mp3" "$title.mp3"; done < title.txt;
 rm title.txt;
 rm result.txt;
