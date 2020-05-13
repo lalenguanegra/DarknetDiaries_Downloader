3582ebf3843f8cb60c41a3192afb09b777a33f85
Hunt The Alien <36826771+lalenguanegra@users.noreply.github.com>
2020-05-12 20:08:09 -0700

    Add files via upload

---
+++ Update_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "ENTER A URL";
+read myInput;
+curl $myInput > html.txt;
+mv html.txt html.txt.gz;
+gunzip html.txt.gz;
+grep -o '"https://traffic.megaphone[^"]*["mp3"]' html.txt | tr -d '"' > result.txt;
+grep -o '"Ep [^"]*["\"\"]' html.txt | tr -d '"' > title.txt;
+rm html.txt;
+while read url; do wget -O $((++i)).mp3 "$url"; done < result.txt;
+while read title; do mv "1.mp3" "$title.mp3"; done < title.txt;
+rm title.txt;
+rm result.txt;
---
+++ Update_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "ENTER A URL";
+for i in {63..64}; do curl "https://darknetdiaries.com/episode/$i/" > html.txt;
+mv html.txt html.txt.gz;
+gunzip html.txt.gz;
+grep -o '"https://traffic.megaphone[^"]*["mp3"]' html.txt | tr -d '"' > result.txt;
+grep -o '"Ep [^"]*["\"\"]' html.txt | tr -d '"' > title.txt;
+rm html.txt;
+while read url; do wget -O $((i)).mp3 "$url"; done < result.txt;
+while read title; do mv "$i.mp3" "$title.mp3"; done < title.txt;
+rm title.txt;
+rm result.txt;
+done;
