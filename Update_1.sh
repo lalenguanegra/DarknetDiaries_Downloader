99d17a124aecc404cabd2591ac26a29709200a4e
Hunt The Alien <36826771+lalenguanegra@users.noreply.github.com>
2020-07-21 07:17:09 -0700

    Update Update_1.sh

--- Update_1.sh
+++ Update_1.sh
@@ -1,3 +1,5 @@
+# Old Code 
+
 #!/bin/bash
 echo "ENTER A URL";
 read myInput;
@@ -11,3 +13,21 @@ while read url; do wget -O $((++i)).mp3 "$url"; done < result.txt;
 while read title; do mv "1.mp3" "$title.mp3"; done < title.txt;
 rm title.txt;
 rm result.txt;
+
+
+# New Code 
+
+#!/bin/bash
+echo "ENTER A URL";
+read myInput;
+curl $myInput > html.txt;
+mv html.txt html.txt.gz;
+gunzip html.txt.gz;
+grep -o '"https://www.podtrac.com[^"]*["mp3"]' html.txt | tr -d '"' > result.txt;
+grep -o '"Ep [^"]*["\"\"]' html.txt | tr -d '"' > title.txt;
+rm html.txt;
+while read url; do wget -O $((++i)).mp3 "$url"; done < result.txt;
+while read title; do mv "1.mp3" "$title.mp3"; done < title.txt;
+rm title.txt;
+rm result.txt;
+
