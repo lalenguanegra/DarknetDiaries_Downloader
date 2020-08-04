76af78d05c95f490a94a7933c66a5f459c81813a
Hunt The Alien <36826771+lalenguanegra@users.noreply.github.com>
2020-08-04 09:13:58 -0700

    Update Update_2.sh

--- Update_2.sh
+++ Update_2.sh
@@ -30,7 +30,7 @@ rm title.txt;
 rm result.txt;
 done;
 
-#Combined Episodes (1 - 70) (Bulk Download)
+#Combined Episodes (1 - 71) (Bulk Download)
 
 #!/bin/bash
 for i in {1..69}; do curl "https://darknetdiaries.com/episode/$i/" > html.txt;
@@ -44,7 +44,7 @@ while read title; do mv "$i.mp3" "$title.mp3"; done < title.txt;
 rm title.txt;
 rm result.txt;
 done;
-for i in {70..70}; do curl "https://darknetdiaries.com/episode/$i/" > html.txt;
+for i in {70..71}; do curl "https://darknetdiaries.com/episode/$i/" > html.txt;
 mv html.txt html.txt.gz;
 gunzip html.txt.gz;
 grep -o '"https://www.podtrac.com[^"]*["mp3"]' html.txt | tr -d '"' > result.txt;
